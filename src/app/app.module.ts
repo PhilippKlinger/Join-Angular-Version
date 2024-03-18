23cc00d0a47f2d876e1b2eb92ed8f72223d244d3
Philipp Klinger <klinger.breuber@web.de>
2024-03-18 18:30:47 +0200

    generated all required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { SummaryComponent } from './main-content/summary/summary.component';
+import { BoardComponent } from './main-content/board/board.component';
+import { AddTaskComponent } from './main-content/add-task/add-task.component';
+import { ContactsComponent } from './main-content/contacts/contacts.component';
+import { HeaderComponent } from './main-content/header/header.component';
+import { ShowTaskComponent } from './dialogs/show-task/show-task.component';
+import { EditTaskComponent } from './dialogs/edit-task/edit-task.component';
+import { ShowContactComponent } from './dialogs/show-contact/show-contact.component';
+import { EditContactComponent } from './dialogs/edit-contact/edit-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SidebarComponent,
+    ImprintComponent,
+    SummaryComponent,
+    BoardComponent,
+    AddTaskComponent,
+    ContactsComponent,
+    HeaderComponent,
+    ShowTaskComponent,
+    EditTaskComponent,
+    ShowContactComponent,
+    EditContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dialogs/show-contact/show-contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-contact',
+  templateUrl: './show-contact.component.html',
+  styleUrls: ['./show-contact.component.scss']
+})
+export class ShowContactComponent {
+
+}
